0051b00f7edc698ff128c267b0fbd848a165959e
sekiyakenji <62208490+sekiyakenji@users.noreply.github.com>
2020-03-19 01:17:34 +0900

    Add files via upload

---
+++ copy.py
@@ -0,0 +1,16 @@
+import os
+import shutil
+import traceback
+#f = open(r'C:\Users\UserName\Desktop\test.json','r',encoding="utf-8")
+
+try:
+    FROMDIR = 'C:/tmp/aa/'
+    TODIR   = 'C:/tmp/bb/'
+    
+    files = os.listdir(FROMDIR)
+    for f in files:
+        shutil.copy  (FROMDIR +  f, TODIR  + f)
+
+except:
+    print((traceback.print_exc()))
+
